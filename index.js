4aa221e4af061a385165897090d3d8194b5adc61
Abhishek Yadav <yugyadav3117624@gmail.com>
2024-05-22 22:21:59 +0530

    added files

---
+++ index.js
@@ -0,0 +1,9 @@
+const box = document.getElementById('container');
+box.style.backgroundColor = "yellow";
+box.style.color = "blue";
+box.style.margin ="20px";
+box.style.padding ="10px";
+box.style.fontSize = "18px";
+box.style.fontWeight= "bold";
+box.style.height ="200px";
+box.style.width ="300px";
